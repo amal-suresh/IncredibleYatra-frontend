f02af362de8d5e51e1663b03e6050eb6e0e9d592
Amal-Suresh <amalsureshms2001@gmail.com>
2025-04-09 09:40:37 +0530

    sidebar imported

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./SideBar";
+import Sidebar from "./Sidebar"
+
 
 const AdminLayout = () => {
   return (
     <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
+      <Sidebar/>
       <main className="flex-1 p-4 sm:ml-64 mb-16 sm:mb-0">
-        <Outlet /> {/* All admin pages render here */}
+        <Outlet /> 
       </main>
     </div>
   );
